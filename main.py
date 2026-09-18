98bc3b8cce5b5798625a706ec340d41407dc4903
cyril <cyril.mazauric@ynov.com>
2026-09-18 18:25:58 +0200

    [ADD] User class & POST route

---
+++ main.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from pydantic import BaseModel, Field, field_validator
+from fastapi import FastAPI
+
+class AccountType(str, Enum):
+    INDIVIDUAL = "individual"
+    PROFESSIONAL = "professional"
+
+class User(BaseModel):
+    id: int
+    username: str = Field(min_length=3, max_length=30)
+    email: str
+    @field_validator("email")
+    @classmethod
+    def check_email(cls, value):
+        if " " in value:
+            raise ValueError("L'email ne doit pas contenir d'espace")
+        return value
+
+    
+    phone: str | None = None
+    account_type: AccountType
+    is_active: bool = True
+
+users_db: dict[int, User] = {}
+
+app = FastAPI()
+
+@app.post("/users")
+def create_user(user: User):
+    users_db[user.id] = user
+    return user
